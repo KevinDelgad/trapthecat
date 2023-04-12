46ff340c195036ee29ee19001d5b48fa3fa1062f
Kevin Delgado <kevindel891@gmail.com>
2023-04-12 14:37:48 -0700

    create simple create-account endpoint

--- user.py
+++ user.py
@@ -3,23 +3,50 @@ import os
 from flask import Flask, render_template, request, url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy.sql import function
+import dataclasses
+
+
+
+@dataclasses.dataclass
+class Newuser:
+    first_name: str
+    last_name: str
+    user_name: str
+    password: str
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, '/var/database.db')
+db = SQLAlchemy()
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-@app.route("/")
+class User(db.Model):
+    username = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=["GET"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Welcome to Trap the Cat!</p>"
 
-@app.route("/create-account")
+@app.route("/create-account", methods=["POST"])
 def create_account():
-    return 200
+    values = request.get_json()
+    user = User(
+        username = values["user"],
+        password = values["password"],
+        first_name = values["first_name"],
+        last_name = values["last_name"]
+    )
+    db.session.add(user)
+    db.session.commit()
+
+    return user.username
