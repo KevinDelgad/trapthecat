35316fc3e5d70d02f21db1022c28a74307182a1c
Kevin Delgado <kevindel891@gmail.com>
2023-04-11 23:36:33 -0700

    remove unneeded files

--- user.py
+++ user.py
@@ -5,8 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import function
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, '/var/database.db')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/create-account")
+def create_account():
+    return 200
